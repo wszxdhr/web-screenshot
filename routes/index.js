5c17a51e43a58fd40b74001af3e57bb1dc3d2d91
张潇 <wszxdhr@126.com>
2017-10-12 22:43:59 +0800

    基本服务完成

--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,37 @@ function computeQuery (query) {
   queryObj.type = (query.type === 'jpg' ? 'jpeg' : query.type)
   query.quality ? queryObj.quality = parseFloat(query.quality) : 100
   query.fullPage ? queryObj.fullPage = JSON.parse(query.fullPage) : ''
-  query.x ? queryObj.x = parseInt(query.x) : ''
-  query.y ? queryObj.y = parseInt(query.y) : ''
-  query.width ? queryObj.width = parseInt(query.width) : ''
-  query.height ? queryObj.height = parseInt(query.height) : ''
-  query.omitBackground ? queryObj.omitBackground = JSON.parse(query.omitBackground) : ''
+  // 截图范围，x,y,width,height必须给全, 且不可出现fullPage，omitBackground选填
+  if (!queryObj.fullPage && query.x && query.y && query.width && query.height) {
+    queryObj.clip = {
+      x: parseInt(query.x),
+      y: parseInt(query.y),
+      width: parseInt(query.width),
+      height: parseInt(query.height)
+    }
+    query.omitBackground ? queryObj.clip.omitBackground = JSON.parse(query.omitBackground) : ''
+  }
   return queryObj
 }
 
 router.get('/', async (ctx, next) => {
+  let timer = new Date().valueOf()
   let requestUrl = ctx.query.src || ''
   let query = computeQuery(ctx.query)
+  console.log(query)
 
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   })
+  console.log(`打开浏览器：${new Date().valueOf() - timer} ms`)
   const page = await browser.newPage()
+  // 设置viewport
+  await page.setViewport({width: 1920, height: 1080})
+  console.log(`打开标签：${new Date().valueOf() - timer} ms`)
   await page.goto(requestUrl)
+  console.log(`打开网页：${new Date().valueOf() - timer} ms （${requestUrl}）`)
   let buffer = await page.screenshot(query)
+  console.log(`截图：${new Date().valueOf() - timer} ms`)
 
   await browser.close()
   ctx.res.writeHead(200, {

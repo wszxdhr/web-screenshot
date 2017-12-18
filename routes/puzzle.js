87ad4683bc55011475c1fb563cc0dabb86b47319
张潇 <wszxdhr@126.com>
2017-12-18 21:59:30 +0800

    edit puzzle get method

--- routes/puzzle.js
+++ routes/puzzle.js
@@ -1,12 +1,18 @@
 const router = require('koa-router')()
+let md5 = require('md5')
+let fs = require('fs')
+let path = require('path')
 
-router.get('/puzzle', async (ctx, next) => {
-  await ctx.render('puzzle.pug')
+router.get('/puzzle/:id', async (ctx, next) => {
+  console.log(ctx.params.id)
+  let elements = fs.readFileSync(path.join(__dirname, '../public/files/temp', `${ctx.params.id}.json`)).toString()
+  ctx.body = elements
 })
 
 router.post('/puzzle', async (ctx, next) => {
-  console.log(JSON.stringify(ctx.request.body))
-  await ctx.render('puzzle.pug', {elements: JSON.stringify(ctx.request.body)})
+  let filename = md5(ctx.request.body)
+  fs.writeFileSync(path.join(__dirname, '../public/files/temp', `${filename}.json`), JSON.stringify(ctx.request.body))
+  ctx.body = {id: filename, url: `http://screenshot.anymelon.com/puzzle`, status: 0}
 })
 
 module.exports = router

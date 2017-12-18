aa1f84396c0a9ed23e0812aeefd97b7405b24e15
张潇 <wszxdhr@126.com>
2017-12-18 20:25:56 +0800

    add puzzle post method

--- routes/puzzle.js
+++ routes/puzzle.js
@@ -4,5 +4,9 @@ router.get('/puzzle', async (ctx, next) => {
   await ctx.render('puzzle.pug')
 })
 
+router.post('/puzzle', async (ctx, next) => {
+  console.log(JSON.stringify(ctx.request.body))
+  await ctx.render('puzzle.pug', {elements: JSON.stringify(ctx.request.body)})
+})
 
 module.exports = router

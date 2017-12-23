cf9141bb622bd97313d67f23b7520f8fc3dc0f46
张潇 <wszxdhr@126.com>
2017-12-23 12:49:07 +0800

    修复json存储问题

--- routes/puzzle.js
+++ routes/puzzle.js
@@ -24,7 +24,7 @@ router.post('/puzzle', async (ctx, next) => {
   let filename = `/files/puzzles/${md5(ctx.request.body + timer)}.png`
   let jsonFile = `/files/temp/${md5(ctx.request.body + timer)}.json`
   let html = pug.renderFile('views/puzzle.pug', ctx.request.body)
-  fs.writeFile(path.join(__dirname, '../public', jsonFile), ctx.request.body)
+  fs.writeFile(path.join(__dirname, '../public', jsonFile), JSON.stringify(ctx.request.body))
 
   console.log(`打开浏览器：${new Date().valueOf() - timer} ms`)
   const page = await browser.newPage()

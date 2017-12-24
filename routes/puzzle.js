984ffc5a7244bc77236449ecca8784950aa54295
张潇 <wszxdhr@126.com>
2017-12-24 21:15:19 +0800

    添加html预览

--- routes/puzzle.js
+++ routes/puzzle.js
@@ -24,8 +24,10 @@ router.post('/puzzle', async (ctx, next) => {
   let puzzleId = md5(ctx.request.body + timer)
   let filename = `/files/puzzles/${puzzleId}.png`
   let jsonFile = `/files/temp/${puzzleId}.json`
+  let htmlFile = `/files/temp/${puzzleId}.html`
   let html = pug.renderFile('views/puzzle.pug', ctx.request.body)
   fs.writeFile(path.join(__dirname, '../public', jsonFile), JSON.stringify(ctx.request.body))
+  fs.writeFile(path.join(__dirname, '../public', htmlFile), html)
 
   console.log(`打开浏览器：${new Date().valueOf() - timer} ms`)
   const page = await browser.newPage()
